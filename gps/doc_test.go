314cb1a9e890c317ccba8797fed53019611d13b9
Bjorn Gilstad <bjorn.gilstad@tihlde.org>
2015-01-10 20:13:46 +0100

    Add example for checksum.

--- gps/doc_test.go
+++ gps/doc_test.go
@@ -2,6 +2,8 @@ package gps
 
 import (
 	"fmt"
+	"log"
+	"strings"
 )
 
 func ExampleGet() {
@@ -16,5 +18,19 @@ func ExampleGet() {
 
 	fmt.Printf("%+v\n", readableData)
 	// Output: Location object with parsed data.
+
+}
+
+func ExampleChecksum() {
+	// Data recieved from GPS.
+	gprmcData := " $GPRMC,194509.000,A,4042.6142,N,07400.4168,W,2.03,221.11,160412,,,A*77"
+
+	// Test checksum
+	if Checksum(gprmcData) {
+		log.Println("Checksum correct")
+	} else {
+		log.Println("Checksum failed")
+
+	}
 
 }
