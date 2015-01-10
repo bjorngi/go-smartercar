3e4a91979882493847ca1a1cf57dab868c235208
Bjorn Gilstad <bjorn.gilstad@tihlde.org>
2015-01-10 20:47:36 +0100

    Remove serial test. Not testable.

--- serial/serial_test.go
+++ serial/serial_test.go
@@ -1,25 +1,8 @@
 package serial
 
 import (
-	"fmt"
 	"testing"
-	"time"
 )
 
 func TestSerial(t *testing.T) {
-	ch := make(chan string)
-	go ReadArduino(ch)
-	time.Sleep(2 * time.Second)
-
-	//	for i := 0; i < 100; i++ {
-	//		if i < 0 {
-	//			continue
-	//		}
-	//
-
-	for {
-		data := <-ch
-		fmt.Printf("%v\n", data)
-	}
-
 }
