34e39769b275fada0220250a0a8091443d0b4635
Bjorn Gilstad <bjorn.gilstad@tihlde.org>
2015-01-10 19:06:44 +0100

    Selected method for example.

--- doc_test.go
+++ doc_test.go
@@ -6,11 +6,11 @@ import (
 )
 
 // Example
-func ExampleExamples() {
+func ExampleGet() {
 	gprmcData := " $GPRMC,194509.000,A,4042.6142,N,07400.4168,W,2.03,221.11,160412,,,A*77"
 	gprmcSplit := strings.Split(gprmcData, "'")
 	readableData, _ := Get(gprmcSplit)
 	fmt.Printf("%+v\n", readableData)
-	//Output: Location object with parsed data.
+	// Output: Location object with parsed data.
 
 }
