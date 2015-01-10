c187420a20f0a186e6aaa68273910cef6f5628ac
Bjorn Gilstad <bjorn.gilstad@tihlde.org>
2015-01-10 18:57:51 +0100

    Move examples.

---
+++ doc_test.go
@@ -0,0 +1,16 @@
+package gps
+
+import (
+	"fmt"
+	"testing"
+)
+
+// Example
+func ExampleExamples() {
+
+	gprmcData := " $GPRMC,194509.000,A,4042.6142,N,07400.4168,W,2.03,221.11,160412,,,A*77"
+	gprmcSplit := strings.Split(gprmcData, "'")
+	readableData, _ := Get(gprmcSplit)
+	fmt.Printf("%+v\n", readableData)
+
+}
--- gps/gps_test.go
+++ gps/gps_test.go
@@ -44,13 +44,3 @@ func TestConvertSpeed(t *testing.T) {
 // Benchmarking
 func BenchmarkParsing(*testing.B) {
 }
-
-// Example
-func ExampleExamples() {
-
-	gprmcData := " $GPRMC,194509.000,A,4042.6142,N,07400.4168,W,2.03,221.11,160412,,,A*77"
-	gprmcSplit := strings.Split(gprmcData, "'")
-	readableData, _ := Get(gprmcSplit)
-	fmt.Printf("%+v\n", readableData)
-
-}
