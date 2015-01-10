f461ad186559b3969409b9ef7a0cc1294e080e50
Bjorn Gilstad <bjorn.gilstad@tihlde.org>
2015-01-10 19:40:12 +0100

    Example test.

--- doc_test.go
+++ doc_test.go
@@ -2,15 +2,25 @@ package gps
 
 import (
 	"fmt"
-	"testing"
 )
 
-// Example
 func ExampleGet() {
+	// Data recieved from GPS.
 	gprmcData := " $GPRMC,194509.000,A,4042.6142,N,07400.4168,W,2.03,221.11,160412,,,A*77"
+
+	// Split data on "," to seperate alle values.
 	gprmcSplit := strings.Split(gprmcData, "'")
+
+	// gps.Get takes []string generated from stings.Split.
 	readableData, _ := Get(gprmcSplit)
+
 	fmt.Printf("%+v\n", readableData)
 	// Output: Location object with parsed data.
+
+}
+
+func Example() {
+	fmt.Printf("%v", "Test!")
+	// Output: Test!
 
 }
